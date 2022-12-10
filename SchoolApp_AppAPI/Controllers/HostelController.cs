cd7db28ad74b7c513f462c49008de7d0e2951158
samuelbanabas <sam_banabas@yahoo.com>
2022-12-10 18:51:22 +0100

    Updated on 10 Dec 2022

---
+++ SchoolApp_AppAPI/Controllers/HostelController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolApp_AppAPI.Data.DatabaseContext;
+using SchoolApp_AppAPI.Models;
+
+namespace SchoolApp_AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HostelController : ControllerBase
+    {
+        private readonly SchoolDBContext _context;
+
+        public HostelController(SchoolDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Hostel
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Hostel>>> GetHostels()
+        {
+            return await _context.Hostels.ToListAsync();
+        }
+
+        // GET: api/Hostel/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hostel>> GetHostel(int id)
+        {
+            var hostel = await _context.Hostels.FindAsync(id);
+
+            if (hostel == null)
+            {
+                return NotFound();
+            }
+
+            return hostel;
+        }
+
+        // POST: api/Hostel
+        [HttpPost]
+        public async Task<ActionResult<Hostel>> PostHostel(Hostel hostel)
+        {
+            _context.Hostels.Add(hostel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHostel", new { id = hostel.Id }, hostel);
+        }
+
+        // DELETE: api/Hostel/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Hostel>> DeleteCourse(int id)
+        {
+            var hostel = await _context.Hostels.FindAsync(id);
+            if (hostel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Hostels.Remove(hostel);
+            await _context.SaveChangesAsync();
+
+            return hostel;
+        }
+        //// PUT: api/Hostel/5
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutHostel(int id, Hostel hostel)
+        //{
+        //    if (id != hostel.Id)
+        //    {
+        //        return
+    }
+}
---
+++ SchoolApp_AppAPI/Controllers/TutorController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolApp_AppAPI.Data.DatabaseContext;
+using SchoolApp_AppAPI.Models;
+
+namespace SchoolApp_AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TutorController : ControllerBase
+    {
+        private readonly SchoolDBContext _context;
+
+        public TutorController(SchoolDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tutor
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tutor>>> GetTutors()
+        {
+            return await _context.Tutors.ToListAsync();
+        }
+
+        // GET: api/Tutor/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tutor>> GetTutor(int id)
+        {
+            var tutor = await _context.Tutors.FindAsync(id);
+
+            if (tutor == null)
+            {
+                return NotFound();
+            }
+
+            return tutor;
+        }
+
+        // POST: api/Tutor
+        [HttpPost]
+        public async Task<ActionResult<Tutor>> PostTutor(Tutor tutor)
+        {
+            _context.Tutors.Add(tutor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTutor", new { id = tutor.Id }, tutor);
+        }
+
+        // DELETE: api/Tutor/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Tutor>> DeleteCourse(int id)
+        {
+            var tutor = await _context.Tutors.FindAsync(id);
+            if (tutor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tutors.Remove(tutor);
+            await _context.SaveChangesAsync();
+
+            return tutor;
+        }
+    }
+ }
